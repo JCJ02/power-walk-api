fa048b0fced07fa8ad1a7c1abb18d7e50c86a666
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-02-09 23:48:26 +0800

    Feat: Appy coerce in UID ZOD Validation

--- src/utils/zod/StudentSchema.ts
+++ src/utils/zod/StudentSchema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 const createStudentSchema = z.object({
-    uid: z.number().min(1, "RFID is Required!"),
+    uid: z.coerce.number({
+        invalid_type_error: "Description Must Be Numbers!"
+    }).min(1, "RFID is Required!"),
     studentId: z.string()
         .min(1, "Student ID is Required!"),
     firstname: z.string()
